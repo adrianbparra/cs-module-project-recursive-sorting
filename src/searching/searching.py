5aedfb5cff35a1f0ded70618302291afa50c22be
adrianbparra <adrianplane12@gmail.com>
2020-10-02 20:51:14 -0700

    updated the binary search. passed test but if value is not there it goes to max recursion

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,26 +1,28 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
-    middle = (start+ end) // 2 
-    # start at the middle
-    # check if target is bigger than or less than
-    if arr == []:
+    
+    if end >= start:
+
+        # start at the middle
+        middle = (start+ end) // 2 
+        # check if target is bigger than or less than
+        if target == arr[middle]:
+            return middle
+            # if greater set new start
+        elif target > arr[middle]:
+            # print("greater")
+            return binary_search(arr,target,middle - 1,end)
+            # if less set new end
+        else:
+            # print("less")
+            return binary_search(arr,target,start,middle + 1)
+    else:
         return -1
 
-    if target == arr[middle]:
-        return middle
-        # if greater set new end
-    elif target > arr[middle]:
-        # print("greater")
-        return binary_search(arr,target,middle,end)
-        # if less set new start
-    else:
-        # print("less")
-        return binary_search(arr,target,start,middle)
+arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
 
-# arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
-
-# print(binary_search(arr1, 2, 0, len(arr1)-1))
+print(binary_search(arr1, -3, 0, len(arr1)-1))
 
 
 # STRETCH: implement an order-agnostic binary search
